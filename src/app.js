da72fdf47a917dde3f1acaec5a17b7fc3c7ca3d8
Student_105 <student_105@ool-ad020213.dyn.optonline.net>
2018-03-30 14:29:37 -0400

    fixing collision

--- src/app.js
+++ src/app.js
@@ -40,7 +40,51 @@ $(document).ready(function () {
   }
 
 
-  function collisionDetection(rectone, recttwo) {
+  // function collisionDetection(rectone, recttwo) {
+
+  //   let r1 = $(rectone);
+  //   let r2 = $(recttwo);
+  //   let r1x = r1.offset().left;
+  //   let r1w = r1.width();
+  //   let r1y = r1.offset().top;
+  //   let r1h = r1.height();
+
+  //   let r2x = r2.offset().left;
+  //   let r2w = r2.width();
+  //   let r2y = r2.offset().top;
+  //   let r2h = r2.height();
+
+  //   if (r1y + r1h < r2y ||
+  //       r1y > r2y + r2h ||
+  //       r1x > r2x + r2w ||
+  //       r1x + r1w < r2x) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+
+  // $(document).keyup(function (e) {
+  //   $('.dog').css({
+  //     top: e.pageY,
+  //     left: e.pageX,
+  //   });
+
+  //   $('.critter').each(function () {
+  //     if (collisionDetection('.dog', $(this))) {
+  //         critter.addClass('deathAnimation');
+  //         score+=1;
+  //         addScore();
+  //     } else if ($(this).hasClass('deathAnimation')) {
+  //       $(this).removeClass('deathAnimation');
+  //       $(this).css('display', 'none');
+  //     }
+  //   });
+  // });
+
+
+
+function collisionDetection(rectone, recttwo) {
 
     let r1 = $(rectone);
     let r2 = $(recttwo);
@@ -64,25 +108,22 @@ $(document).ready(function () {
     }
   }
 
-  $(document).keyup(function(e) {
-    $('.dog').css({
-      top: e.pageY,
-      left: e.pageX,
-    });
-
+  $(document).keyup(function (e) {
     $('.critter').each(function () {
       if (collisionDetection('.dog', $(this))) {
-          critter.addClass('deathAnimation');
-          score+=1;
-          addScore();
+        $(this).addClass('deathAnimation');
+        score+=1;
+        addScore();
       } else if ($(this).hasClass('deathAnimation')) {
         $(this).removeClass('deathAnimation');
         $(this).css('display', 'none');
+      // } else if (collisionDetection('.critter', '.dog')){
+      //   console.log('i hit you instead!')
       }
     });
   });
 
-  //setInterval(collisionDetection, );
+  setInterval(collisionDetection, 250);
 
   function addScore() {
     scoreBoard.css('font-size', '20px');
