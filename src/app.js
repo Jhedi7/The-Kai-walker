1533ce654347f6dded66a508076be1e925cfff91
Student_105 <student_105@ool-ad020213.dyn.optonline.net>
2018-04-02 17:08:46 -0400

    5pm

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 
-  $('.gameContainer').scroll(function() {
+$('.gameContainer').scroll(function() {
 
     $('.dog').css('top', $(this).scrollTop());
 });
@@ -50,96 +50,10 @@ window.onload = function () {
   }
 
 
-  // function collisionDetection(rectone, recttwo) {
-
-  //   let r1 = $(rectone);
-  //   let r2 = $(recttwo);
-  //   let r1x = r1.offset().left;
-  //   let r1w = r1.width();
-  //   let r1y = r1.offset().top;
-  //   let r1h = r1.height();
-
-  //   let r2x = r2.offset().left;
-  //   let r2w = r2.width();
-  //   let r2y = r2.offset().top;
-  //   let r2h = r2.height();
-
-  //   if (r1y + r1h < r2y ||
-  //       r1y > r2y + r2h ||
-  //       r1x > r2x + r2w ||
-  //       r1x + r1w < r2x) {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
-
-  // $(document).keyup(function (e) {
-  //   $('.dog').css({
-  //     top: e.pageY,
-  //     left: e.pageX,
-  //   });
-
-  //   $('.critter').each(function () {
-  //     if (collisionDetection('.dog', $(this))) {
-  //         critter.addClass('deathAnimation');
-  //         score+=1;
-  //         addScore();
-  //     } else if ($(this).hasClass('deathAnimation')) {
-  //       $(this).removeClass('deathAnimation');
-  //       $(this).css('display', 'none');
-  //     }
-  //   });
-  // });
 
 
 
-// function collisionDetection(rectone, recttwo) {
-
-//     let r1 = $(rectone);
-//     let r2 = $(recttwo);
-//     let r1x = r1.offset().left;
-//     let r1w = r1.width();
-//     let r1y = r1.offset().top;
-//     let r1h = r1.height();
-
-//     let r2x = r2.offset().left;
-//     let r2w = r2.width();
-//     let r2y = r2.offset().top;
-//     let r2h = r2.height();
-
-//     if (r1y + r1h < r2y ||
-//         r1y > r2y + r2h ||
-//         r1x > r2x + r2w ||
-//         r1x + r1w < r2x) {
-
-//     console.log('also here')
-//     //debugger;
-//     } else {
-//       //return true;
-//       console.log('here');
-//     }
-//   }
-
-//   setInterval(function (e) {
-//     $('.critter').each(function () {
-//       if (collisionDetection('.dog', $(this))) {
-//         $(this).addClass('deathAnimation');
-//         score+=1;
-//         addScore();
-//       } else if ($(this).hasClass('deathAnimation')) {
-//         $(this).removeClass('deathAnimation');
-//         $(this).css('display', 'none');
-//       // } else if (collisionDetection('.critter', '.dog')){
-//       //   console.log('i hit you instead!')
-//       }
-//     });
-//   });
-//   250
-
-
-
-  window.setInterval(collisionDetection, 250);
+  window.setInterval(collisionDetection, );
 
   function collisionDetection() {
     let dogWidth, dogHeight, dogX, dogY, critterWidth, critterHeight, critterX, critterY;
@@ -177,28 +91,7 @@ $('.critter').each(function () {
       }
     });
 
- // $('.critter').each(function () {
- //      if (collisionDetection('.dog', $(this))) {
- //        $(this).addClass('deathAnimation');
 
- //        score+=1;
- //        addScore();
- //      } else if ($(this).hasClass('deathAnimation')) {
- //        $(this).removeClass('deathAnimation');
- //        $(this).css('display', 'none');
-
- //      }
- //    });
-
-// var hitting = setInterval(function () {
-//             var treeHit = false;
-//             $('.critter').each(function () {
-//                 treeHit = collisionDetection(dog, $(this));
-//                 if (treeHit == true) {
-//                     console.log('hit2');
-//                 }
-//             });
-//         }, 1);
 
 
 
@@ -217,7 +110,11 @@ $('.critter').each(function () {
 
   // squirrel();
 
-
+function formName() {
+    let input = document.getElementById("nameSubmit").value;
+    alert(input);
+    console.log('anything')
+}
 
 
 };
