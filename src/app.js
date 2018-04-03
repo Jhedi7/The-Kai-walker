c4f7a3b539057f680d9466b87b607beda4188d01
Student_105 <student_105@PSXGA105.home>
2018-04-02 23:35:30 -0400

    final submission

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 window.onload = function () {
 
 
-
-$('.gameContainer').scroll(function() {
-
-    $('.dog').css('top', $(this).scrollTop());
-});
-
   const gameContainer = $('.gameContainer')
+  const containerLocation = gameContainer[0].getBoundingClientRect()
   let dog = $('.dog');
   let critter = $('.critter');
   let score = 0;
   let scoreBoard = $('.scoreNumber');
-  const critterCollection = document.getElementsByClassName('critter')
+  const critterCollection = document.getElementsByClassName('critter');
+
+  $('.gameContainer').scroll(function () {
+    $('.dog').css('top', $(this).scrollTop());
+  });
 
 
 $(document).keydown(function(event) {
@@ -50,9 +49,11 @@ $(document).keydown(function(event) {
   function choose() {
     for (let i = 0; i < critterCollection.length; i++) {
       setInterval(collision(dog[0].getBoundingClientRect(),
-        critterCollection[j].getBoundingClientRect()), 250)
+        critterCollection[i].getBoundingClientRect()
+      ), 250);
     }
   }
+
   setInterval(choose, 250);
 
   function collision(rect1, rect2) {
@@ -60,27 +61,67 @@ $(document).keydown(function(event) {
       rect1.x + rect1.width > rect2.x &&
        rect1.y < rect2.y + rect2.height &&
         rect1.height + rect1.y > rect2.y) {
-      console.log('collision')
+      console.log('collision');
     }
   }
 
 
 
+  for (let i = 0; i < 4; i++) {
+    makeCritters();
+  }
 
 
+  function makeCritters() {
+    let critters = $('<img></img>').attr({
+      'class': 'critter',
+      'src': 'tails.gif'
+    })
+    let critters2 = $('<img></img>').attr({
+      'class': 'critter2',
+      'src': 'tails.gif'
+    })
+    let critters3 = $('<img></img>').attr({
+      'class': 'critter3',
+      'src': 'tails.gif'
+    })
+    let top = Math.random() * containerLocation.height
+    let left = Math.random() * containerLocation.width
+    gameContainer.append(critters2)
+    gameContainer.append(critters)
+    gameContainer.append(critters3)
+    $(critters2).css({
+      'top': top,
+      //'left': left
+    })
+    $(critters).css({
+      //'top': top,
+      'left': left
+    })
+    $(critters3).css({
+      'top': top,
+      'left': left
+    })
+  }
 
   function addScore() {
     scoreBoard.css('font-size', '20px');
     scoreBoard.html(score);
+
   }
+  addScore();
 
+let nameSubmit = $('#nameSubmit')
 
-
-function formName() {
-    let input = document.getElementById("nameSubmit").value;
-    alert(input);
-    console.log('anything')
-}
+ $(document).on('click', nameSubmit, function(){
+   let userName =  $('#inputName').val();
+   alert(userName);
+ });
+// function formName() {
+//     let input = document.getElementById('nameSubmit').value;
+//     alert(input);
+//     console.log('anything');
+//  }
 
 
 };
